0cc036ea1d6bb33aa480f0e9846ad47181ee10ad
olton <serhii@pimenov.com.ua>
2024-10-03 12:09:51 +0300

    add blog

--- src/js/email.js
+++ src/js/email.js
@@ -1,9 +1,9 @@
-globalThis.sendRequestForAdsBlock = async (form) => {
+globalThis.sendRequestForm = async (form) => {
     const result = await Email.send({
         SecureToken : TOKEN,
         To : 'serhii@pimenov.com.ua',
-        From : "site@trysusidy.kyiv.ua",
-        Subject : "ОСББ Три сусіди 1 - Запит на створення об'яви",
+        From : "site@pimenov.com.ua",
+        Subject : "Сайт Сергія Піменова - Запит на повідомлення",
         Body : `
     ФІО: ${form["name"].value}<br/>                               
     Телефон: ${form["tel"].value}<br/>                                
@@ -15,52 +15,13 @@ globalThis.sendRequestForAdsBlock = async (form) => {
     -----------------------<br/>
     ${form["description"].value}<br/>
     -----------------------<br/>
-    Лист відправлено з trysusidy.kiev.ua                                
         `
     })
     if (result.toLowerCase() === "ok") {
-        Metro.toast.create("Дякуємо! Об1яву відправлено на розгляд адміністрації сайта.", {
+        Metro.toast.create("Дякуємо! Ваше повідомлення відправлено!.", {
             clsToast: "success",
             showTop: true,
         })
-        setTimeout(() => {
-            window.location.href="/ads"
-        }, 2000)
-    } else {
-        Metro.toast.create(`Помилка! ${result}`, {
-            clsToast: "alert",
-            showTop: true,
-        })
-    }
-}
-
-globalThis.sendAppealForHead = async (form) => {
-    const result = await Email.send({
-        SecureToken : TOKEN,
-        To : 'serhii@pimenov.com.ua',
-        From : "site@trysusidy.kyiv.ua",
-        Subject : "ОСББ Три сусіди 1 - Звернення до голови",
-        Body : `
-    ФІО: ${form["name"].value}<br/>                               
-    Телефон: ${form["tel"].value}<br/>                                
-    Email: ${form["email"].value}<br/>
-    -----------------------<br/>                                
-    Адреса: ${form["house"].value}, кв/оф. ${form["app"].value}<br/>
-    Терміново: ${form["urgently"].checked ? "Так" : "Ні"}<br/>
-    -----------------------<br/>
-    ${form["question"].value}<br/>
-    -----------------------<br/>
-    Лист відправлено з trysusidy.kiev.ua                                
-        `
-    })
-    if (result.toLowerCase() === "ok") {
-        Metro.toast.create("Дякуємо! Ваше звернення відправлено на розгляд.", {
-            clsToast: "success",
-            showTop: true,
-        })
-        setTimeout(() => {
-            window.location.href="/ads"
-        }, 2000)
     } else {
         Metro.toast.create(`Помилка! ${result}`, {
             clsToast: "alert",
